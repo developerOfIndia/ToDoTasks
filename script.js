b700a6c008d65f65b93039de69a06ce52a7522e9
developerOfIndia <130592833+developerOfIndia@users.noreply.github.com>
2025-03-17 14:37:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function addTask(){
+    const inputField = document.getElementById('inputTask')
+    const taskText = inputField.value.trim()
+    if(taskText == ""){
+        alert("Task can not be empty!")
+        inputField.value=""
+        return
+    }
+    const newTask = document.createElement('li')
+    const taskList = document.getElementById('taskList')
+    taskList.appendChild(newTask)
+    newTask.textContent = document.getElementById('inputTask').value
+    document.getElementById('inputTask').value=""
+    deleteTask(newTask)
+}
+function deleteTask(newTask)
+{
+    const deleteBtn = document.createElement('button')
+    deleteBtn.textContent="Delete"
+    newTask.appendChild(deleteBtn)
+    deleteBtn.onclick = function(){
+        newTask.remove()
+    }
+}
